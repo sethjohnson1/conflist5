91ba388d5fc1b2de660a4472034f19c94f82d44f
nj @ gravehold ⬅️ <nilesjohnson@gravehold.privaete.com>
2024-07-30 10:54:19 -0400

    fixes to calendar helpers #4
    - use new format methods for Date objects
    - gcal doesn't use the extra fields from Configure, so delete them
    - delete commented old versions

--- src/View/Helper/GcalHelper.php
+++ src/View/Helper/GcalHelper.php
@@ -6,21 +6,17 @@ use Cake\View\View;
 
 class GcalHelper extends Helper{
     public function initialize(array $config): void {
-       //$site_url = Configure::read('site.home');
-       //$site_name = Configure::read('site.name');
        // debug($config);
     }
     public function gcal_url($id, $start_date, $end_date, $title, $city, $country, $url) {
-      $start_string = str_replace('-','',$start_date);
-      $end_string = date('Ymd',strtotime($end_date." +1 day"));
+      $start_string = $start_date->format('Ymd');
+      $end_string = $end_date->modify('+1 day')->format('Ymd');
       $location = $city."; ".$country;
       $Gcal_url = "http://www.google.com/calendar/event?action=TEMPLATE&".
         "text=".urlencode($title)."&".
         "dates=".$start_string."/".$end_string.
         "&details=".$url.
-        "&location=".urlencode($location).
-        "&trp=false&sprop=".urlencode($url).
-        "&sprop=name:".urlencode($this->site_name);
+        "&location=".urlencode($location);
       return $Gcal_url;
     }
     public function gcal($conference) {
@@ -35,41 +31,4 @@ class GcalHelper extends Helper{
 
     }
 }
-/*
-class GcalHelper extends Helper {
-  public function initialize(array $config) {
-    // /parent::__construct($view, $settings);
-    $site_url = Configure::read('site.home');
-    $site_name = Configure::read('site.name');
-    //debug($settings);
-  }
-  
-
-  public function gcal_url($id, $start_date, $end_date, $title, $city, $country, $url) {
-    $start_string = str_replace('-','',$start_date);
-    $end_string = date('Ymd',strtotime($end_date." +1 day"));
-    $location = $city."; ".$country;
-    $Gcal_url = "http://www.google.com/calendar/event?action=TEMPLATE&".
-      "text=".urlencode($title)."&".
-      "dates=".$start_string."/".$end_string.
-      "&details=".$url.
-      "&location=".urlencode($location).
-      "&trp=false&sprop=".urlencode($this->site_url).
-      "&sprop=name:".urlencode($this->site_name);
-    return $Gcal_url;
-  }
-
-  public function gcal($conference) {
-    return $this->gcal_url($conference['id'], 
-			   $conference['start_date'], 
-			   $conference['end_date'],
-			   $conference['title'],
-			   $conference['city'],
-			   $conference['country'],
-			   $conference['homepage']
-			   );
-
-  }
-}
-*/
 ?>
