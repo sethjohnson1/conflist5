ee733af015326ece0de49c967db5ed22137c3602
Niles Johnson ꙮ <niles@k2ps187.privaete.com>
2025-08-11 16:03:05 +0000

    update maintenance page to use readOnly boolean for #41

--- templates/Conferences/maintenance.php
+++ templates/Conferences/maintenance.php
@@ -3,6 +3,9 @@ use Cake\Core\Configure;
 echo '<h1>'.$view_title.'</h1>';
 ?>
 
+<?php
+    if (Configure::read('readOnly')) {
+        echo "
 <p>The addition of new announcements is temporarily disabled due to an internal bug with confirmation emails.  We hope to have this resolved as soon as possible.</p>
 
 <h2>Beginning of maintenance period</h2>
@@ -11,6 +14,13 @@ echo '<h1>'.$view_title.'</h1>';
 
 <h2>Expected end of maintenance period</h2>
     <p>Within one week</p>
+";
+    }
+    else {
+        echo "<h2>No active maintenance</h2>";
+        echo "<p>The site is currently not under maintenance.  Please <a href='http://nilesjohnson.net/contact.html' target='blank'>contact Niles</a> if you experience any error or irregularity.</p>";
+    }
+?>
 
 <h2>Donations</h2>
 
